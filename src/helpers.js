715f3b3c686b2efb700d97141ea09acd57767f29
andhika-satrio <riouchidou@gmail.com>
2020-09-27 16:58:32 +0700

    memperbaiki incrementQty

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,7 @@
-const incrementQty = (qty) => qty + 1;
+const incrementQty = (qty) => parseInt(qty)+ 1;
 const decrementQty = (qty) => qty - 1;
 const subTotal = (price, qty) => price * qty;
+
 
 function disableDecrement(qty){
     if (qty == 1){
