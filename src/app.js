a5b70a49050a3a3ccd2bc630ac7ded35229c7063
andhika-satrio <riouchidou@gmail.com>
2020-09-27 16:39:35 +0700

    menambahkan subtotal dan price

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import {
 const incrButton = document.querySelector('#incr');
 const decrButton = document.querySelector('#decr');
 const qtyInput = document.querySelector('#qty');
+const txtSubtotal = document.querySelector('#subtotal');
+const priceInput = document.querySelector('#price');
 
 incrButton.addEventListener('click', () => {
   qtyInput.value = incrementQty(qtyInput.value);
