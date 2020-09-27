ba557e8ed154a17d5990d35c5d2432496d41f310
andhika-satrio <riouchidou@gmail.com>
2020-09-27 16:24:57 +0700

    menambahkan testing decrement dan increment

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -2,3 +2,12 @@
     Gunakan file ini untuk membuat test case dengan Jest
 */
 
+const helper = require('../helpers');
+
+test ('Jika qty adalah 2, ketika dikurangi 1 jadi = 1',() =>{
+    expect(helper.decrementQty(2)).toBe(1);
+});
+
+test ('Jika qty adalah 7, ketika ditambah 1 jadi = 8',() =>{
+    expect(helper.incrementQty(7)).toBe(8);
+});
